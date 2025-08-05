332abcda95417ccd372e2e5e5b24752ad7c5eb3c
Leonard Phokane <leonardphokane1@gmail.com>
2025-08-05 14:37:17 +0200

    Add files via upload

---
+++ Typed.js
@@ -0,0 +1,20 @@
+// typed.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typed-leonard", {
+    strings: [
+      "Hello, I'm Leonard Phokane 👨🏾‍💻",
+      "Full Stack Explorer 🚀",
+      "Creative Technologist ✨",
+      "Backend Specialist 🧩",
+      "Afro-futurist Dreamweaver 🌍"
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+    backDelay: 1500,
+    smartBackspace: true,
+    showCursor: true,
+    cursorChar: "|"
+  });
+});
